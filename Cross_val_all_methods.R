df4dcb970d335c13a5f23537766767214d8da779
caitlinkuhlman <caitlinkuhlman@gmail.com>
2018-04-18 20:00:39 -0400

    completed cross val for knn, gam, ls

--- Cross_val_all_methods.R
+++ Cross_val_all_methods.R
@@ -7,18 +7,23 @@ source("util.R")
 # import csv file
 df=read.csv("data_cleaned/training_mortality_ratios.csv", header = TRUE)
 #data
-dim(df)
+#dim(df)
 names(df)
 
+#scaling?
+#df=scale(df)
+#df
 #reorder by column index
 #first col is response
 #include all predictors
-#dfc <- df[c(4,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)]
-#dff <- df[c(5,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)]
-#dfm <- df[c(6,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)]
+dfc <- df[c(4,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)]
+dff <- df[c(5,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)]
+dfm <- df[c(6,2,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)]
 
-dff <- df[c(5,2,14,17,18,19,20,21,22,23,24)]
-dfm <- df[c(6,2,7,14,17,18,19,20,21,22,23,24)]
+#can choose different sets of features
+#dff <- df[c(5,2,14,17,18,19,20,21,22,23,24)]
+#dfm <- df[c(6,2,7,14,17,18,19,20,21,22,23,24)]
+
 #split into tuning and evaluating sets
 set.seed(1234)
 idx = sample(nrow(df), nrow(df)*0.5)
@@ -29,41 +34,21 @@ dff_eval = dff[-idx,1:ncol(dff)]
 dfm_tune = dfm[idx,1:ncol(dfm)]
 dfm_eval = dfm[-idx,1:ncol(dfm)]
 
-dfs = c(dfc, dff, dfm)
+dfs = list(dfc, dff, dfm)
 tunes=list(dfc_tune,dff_tune,dfm_tune)
 evals=list(dfc_eval,dff_eval,dfm_eval)
-head(dfc)
-################################################ KNN ##############################################
-cv_knn<-function(df, num_k, num_folds){
-  #init error vector
-  errors <-rep(0,num_folds)
-  df["idx"]<-get_folds(df,num_folds)
-  for (fold in 1:num_folds){
-    #get training and testing for this fold
-    train <- df[df$idx!=fold,]
-    test <- df[df$idx==fold,]
-    #for methods that need a matrix not dataframe, convert
-    trainf <- train[,3:ncol(train)-1]
-    testf <- test[,3:ncol(train)-1]
-    train_y <- train[,1]
-    test_y <- test[,1]
-    #fit model on training
-    #use to predict testing
-    fit <- knn.reg(train = trainf, test = testf, y = train_y, k = num_k)
-    #compute MSE
-    err <- mean((test_y - fit$pred)^2)
-    #put errors in a vector
-    errors[fold]<-err
-    #print(fold)
-  }
-  return(mean(errors))
-}
+#head(dfc)
 
+
+
+################################################ KNN ##############################################
+
+#ks = c(1,3,5,10,20,30,50,100)
+ks = c(1,3,5,7,9,10,11,15,18,20)
+
+# Tune k
 for(j in 1:length(tunes)){
-  # Tune k
-  #ks = c(1,3,5,10,20,30,50,100)
   df=tunes[[j]]
-  ks = c(1,3,5,7,9,10,11,15,18,20)
   kerrors=rep(0,length(ks))
   for (i in 1:length(ks)){
     kerrors[i]=cv_knn(df, ks[i], 10)
@@ -77,13 +62,12 @@ for(j in 1:length(tunes)){
 #c best 9
 #f best 9
 #m best 10
-11
-9
+
 #7 also good enough prob for all
 
 #best k=7
-#Evaluate 
 
+#Evaluate 
 print("dfc mse")
 print(cv_knn(dfc_eval, 7, 10))
 print("dff mse")
@@ -95,69 +79,88 @@ fit = knn.reg(train=dfc_eval[,3:length(dfc_eval)-1], test=dfc_eval[,3:length(dfc
 #compute MSE
 plot_res(dfc_eval[,1], fit$pred)
 
-#can scale but it doesn't make much difference
-sdf=scale(dfc)
-sdf=dfc
+ks
+ks[which(min(kerrors) == kerrors)[[1]]]
+
+
 
 ################################################ GAM ##############################################
-cv_gam<-function(df, deg, num_folds){
-  #init error vector
-  errors = rep(0,num_folds)
-  df["idx"]=get_folds(df,num_folds)
-  for (fold in 1:num_folds){
-    #get training and testing for this fold
-    train <- df[df$idx!=fold,]
-    test <- df[df$idx==fold,]
-    #for methods that need a matrix not dataframe, convert
-    trainf = train[,3:ncol(train)-1]
-    testf = test[,3:ncol(train)-1]
-    train_y = train[,1]
-    test_y = test[,1]
-    #fit model on training
-    #use to predict testing
-    fit=gam(train_y~trainf[,1]+
-                       s(trainf[,2], deg), data=trainf)  
-    #compute MSE
-    err = mean((test_y - fit$pred)^2)
-    #put errors in a vector
-    errors[fold]=err
-    print(fold)
-  }
-  return(mean(errors))
-}
-errs = cv_gam(dfc, 3, 10)
-errs# Tune k
+
+# Tune degree
 #ks = c(1,3,5,10,20,30,50,100)
-ks = c(1,3,5,7,9,10,11,15,18,20)
-kerrors=rep(0,length(ks))
-ks
-for (i in 1:length(ks)){
-  kerrors[i]=cv_knn(dfc_tune, ks[i], 10)
+degs = c(1,2,3,5,7,9)
+gerrors=rep(0,length(degs))
+for (i in 1:length(degs)){
+  gerrors[i]=cv_gam(dfc_tune, degs[i], 10)
 }
-plot(ks, kerrors, type="l")
+plot(degs, gerrors,type="l")
 
-df=dfc
-df["idx"]=get_folds(df,3)
-fold=3
-train <- df[df$idx!=fold,]
-test <- df[df$idx==fold,]
-#for methods that need a matrix not dataframe, convert
-trainf = train[,3:ncol(train)-1]
-testf = test[,3:ncol(train)-1]
-train_y = train[,1]
-test_y = test[,1]
-fit=gam(train_y~trainf[,1]+
-          s(trainf[,2], 3), data=trainf)
+best_deg = degs[which(min(gerrors) == gerrors)[[1]]]
 
-print(summary(gam.m3))
-print("##############")
-# Mean Squared Error
-print(mean((predict(gam.m3, test) - test$female_mort_y)^2))
+#Evaluate 
+print("dfc mse")
+dfc_mse = cv_gam(dfc_eval, best_deg, 10)
+print(dfc_mse)
+#print("dff mse")
+#print(cv_knn(dff_eval, 7, 10))
+#print("dfm mse")
+#print(cv_knn(dfm_eval, 9, 10))
+
+
+#plot training error
+fit_gam = gam(as.formula(paste(paste(colnames(df)[1], "~ s(", 
+                                     paste(colnames(df)[3:length(df)-1], collapse = 
+                                             paste(",",toString(best_deg), ") + s(", sep=""),sep = "")),
+                               paste(",",toString(best_deg), ")", sep=""))), data=df)
+
+summary(fit_gam)
+par(mfrow=c(4,5))
+plot(fit_gam)
 par(mfrow=c(1,1))
-plot_res(test$female_mort_y,predict(gam.m3, test),as.factor(test$x1_year))
+plot_res(dfc_eval[,1], predict(fit_gam, dfc_eval))
 
-print("knn mse")
-print(cv_knn(dfc_eval, 7, 10))
+
+
+################################################ Least Squares ##############################################
+
+
+
+# Nothing to tune!
+
+#Evaluate 
+print("dfc mse")
+dfc_mse = cv_lm(dfc, 10)
+print(dfc_mse)
+#print("dff mse")
+#print(cv_knn(dff_eval, 7, 10))
+#print("dfm mse")
+#print(cv_knn(dfm_eval, 9, 10))
+
+
+#plot training error
+fit_lm = glm(as.formula(paste(colnames(dfc)[1], "~", 
+                              paste(colnames(dfc)[3:length(dfc)-1], 
+                                    collapse =  "+"), sep="")), data=dfc)
+summary(fit_lm)
+
+#Should drop Days.PM10 ? lm shows it is linear combo of other predictor 
+names(dfc)
+dfc_try = dfc[c(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,19,10)]
+names(dfc_try)
+fit_lm = glm(as.formula(paste(colnames(dfc_try)[1], "~", 
+                              paste(colnames(dfc_try)[3:length(dfc_try)-1], 
+                                    collapse =  "+"), sep="")), data=dfc_try)
+summary(fit_lm)
+par(mfrow=c(2,2))
+plot(fit_lm)
+
+par(mfrow=c(1,1))
+plot_res(dfc[,1], predict(fit_lm, dfc))
+
+
+
+
+
 
 
 
--- util.R
+++ util.R
@@ -7,6 +7,243 @@ plot_res <-function(y, pred){
 
 #this function lets you do CV
 get_folds<-function(X, nfolds){
-  folds = rep(1:nfolds, length.out=nrow(X))  
-  return(sample(folds)) 
+  n = nrow(X)
+  if(n %% nfolds != 0){
+    f = floor(n/nfolds)
+    n=(f+1)*nfolds
+  }
+  folds = rep(1:nfolds, length.out=n)
+  return(sample(folds)[1:nrow(X)]) 
 }
+
+##Cross fold validation for knn
+cv_knn<-function(df, num_k, num_folds){
+  #init error vector
+  errors <-rep(0,num_folds)
+  df["idx"]<-get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf <- train[,3:ncol(train)-1]
+    testf <- test[,3:ncol(train)-1]
+    train_y <- train[,1]
+    test_y <- test[,1]
+    #fit model on training
+    #use to predict testing
+    fit <- knn.reg(train = trainf, test = testf, y = train_y, k = num_k)
+    #compute MSE
+    err <- mean((test_y - fit$pred)^2)
+    #put errors in a vector
+    errors[fold]<-err
+    #print(fold)
+  }
+  return(mean(errors))
+}
+
+##Cross fold validation for gam
+#deg is the degree parameter
+cv_gam<-function(df, deg, num_folds){
+  #init error vector
+  errors = rep(0,num_folds)
+  df["idx"]=get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf = train[,3:ncol(train)-1]
+    testf = test[,3:ncol(train)-1]
+    train_y = train[,1]
+    test_y = test[,1]
+    #fit model on training
+    #use to predict testing
+    fit_gam = gam(as.formula(paste(paste(colnames(df)[1], "~ s(", 
+                                         paste(colnames(df)[3:length(df)-1], collapse = 
+                                                 paste(",",toString(deg), ") + s(", sep=""),sep = "")),
+                                   paste(",",toString(deg), ")", sep=""))), data=df)
+    # Mean Squared Error
+    err = mean((predict(fit_gam, testf) - test_y)^2)  
+    #put errors in a vector
+    errors[fold]=err
+    print(fold)
+  }
+  return(mean(errors))
+}
+
+
+##Cross fold validation for least squares
+cv_lm<-function(df, num_folds){
+  #init error vector
+  errors = rep(0,num_folds)
+  df["idx"]=get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf = train[,3:ncol(train)-1]
+    testf = test[,3:ncol(train)-1]
+    train_y = train[,1]
+    test_y = test[,1]
+    #fit model on training
+    #use to predict testing
+    fit_lm = glm(as.formula(paste(colnames(dfc)[1], "~", 
+                            paste(colnames(dfc)[3:length(dfc)-1], 
+                            collapse =  "+"), sep="")), data=df)
+    # Mean Squared Error
+    err = mean((predict(fit_lm, testf) - test_y)^2)  
+    #put errors in a vector
+    errors[fold]=err
+    print(fold)
+  }
+  return(mean(errors))
+}
+            
+  
+##Cross fold validation for ridge
+#l is the lambda parameter
+cv_ridge<-function(df, l, num_folds){
+  #init error vector
+  errors = rep(0, num_folds)
+  df["idx"]=get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf = train[,3:ncol(train)-1]
+    testf = test[,3:ncol(train)-1]
+    train_y = train[,1]
+    test_y = test[,1]
+    #fit model on training
+    #use to predict testing
+    fit_lm = glm(as.formula(paste(colnames(dfc)[1], "~", 
+                                  paste(colnames(dfc)[3:length(dfc)-1], 
+                                        collapse =  "+"), sep="")), data=df)
+    # Mean Squared Error
+    err = mean((predict(fit_lm, testf) - test_y)^2)  
+    #put errors in a vector
+    errors[fold]=err
+    print(fold)
+  }
+  return(mean(errors))
+}
+
+
+##Cross fold validation for lasso
+#l is the lambda parameter
+cv_lasso<-function(df, l, num_folds){
+  #init error vector
+  errors = rep(0,num_folds)
+  df["idx"]=get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf = train[,3:ncol(train)-1]
+    testf = test[,3:ncol(train)-1]
+    train_y = train[,1]
+    test_y = test[,1]
+    #fit model on training
+    #use to predict testing
+    fit_lm = glm(as.formula(paste(colnames(dfc)[1], "~", 
+                                  paste(colnames(dfc)[3:length(dfc)-1], 
+                                        collapse =  "+"), sep="")), data=df)
+    # Mean Squared Error
+    err = mean((predict(fit_lm, testf) - test_y)^2)  
+    #put errors in a vector
+    errors[fold]=err
+    print(fold)
+  }
+  return(mean(errors))
+}
+
+library(glmnet)
+grid = 10^seq(10, -2, length=100)
+
+#combined mortality
+x = model.matrix(combined_mort_y~.,dataf_c)
+y =dataf$combined_mort_y
+
+ridge.mod = glmnet(x,y,alpha=0, lambda = grid) 
+set.seed(1)
+train = sample(1:nrow(x), nrow(x)/2)
+test  = (-train)
+y.test = y[test]
+ridge.mod = glmnet(x[train,], y[train], alpha=0, lambda=grid, thresh=1e-12)
+ridge.pred=predict(ridge.mod, s=4, newx=x[test,])
+mean((ridge.pred-y.test)^2)
+ridge.pred=predict(ridge.mod, s=1000000, newx=x[test,])
+mean((ridge.pred-y.test)^2)
+
+# use cross-validation to choose lambda
+cv.out = cv.glmnet(x[train,], y[train], alpha=0)
+plot(cv.out)
+bestlam=cv.out$lambda.min
+bestlam
+ridge.pred=predict(ridge.mod, s=bestlam, newx=x[test,])
+mean((ridge.pred-y.test)^2)       
+            
+            
+
+##Cross fold validation for trees
+cv_tree<-function(df, deg, num_folds){
+  #init error vector
+  errors = rep(0,num_folds)
+  df["idx"]=get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf = train[,3:ncol(train)-1]
+    testf = test[,3:ncol(train)-1]
+    train_y = train[,1]
+    test_y = test[,1]
+    #fit model on training
+    #use to predict testing
+    fit_lm = glm(as.formula(paste(colnames(dfc)[1], "~", 
+                                  paste(colnames(dfc)[3:length(dfc)-1], 
+                                        collapse =  "+"), sep="")), data=df)
+    # Mean Squared Error
+    err = mean((predict(fit_lm, testf) - test_y)^2)  
+    #put errors in a vector
+    errors[fold]=err
+    print(fold)
+  }
+  return(mean(errors))
+}
+
+
+##Cross fold validation for random forest
+cv_rf<-function(df, deg, num_folds){
+  #init error vector
+  errors = rep(0,num_folds)
+  df["idx"]=get_folds(df,num_folds)
+  for (fold in 1:num_folds){
+    #get training and testing for this fold
+    train <- df[df$idx!=fold,]
+    test <- df[df$idx==fold,]
+    #for methods that need a matrix not dataframe, convert
+    trainf = train[,3:ncol(train)-1]
+    testf = test[,3:ncol(train)-1]
+    train_y = train[,1]
+    test_y = test[,1]
+    #fit model on training
+    #use to predict testing
+    fit_lm = glm(as.formula(paste(colnames(dfc)[1], "~", 
+                                  paste(colnames(dfc)[3:length(dfc)-1], 
+                                        collapse =  "+"), sep="")), data=df)
+    # Mean Squared Error
+    err = mean((predict(fit_lm, testf) - test_y)^2)  
+    #put errors in a vector
+    errors[fold]=err
+    print(fold)
+  }
+  return(mean(errors))
+}
+            
+            
